8903c3af518ba0541c11fad718f3384acb965870
Himanshu Masant <himanshu.masant@gmail.com>
2024-08-27 12:25:41 +0530

    updates

--- bluetooth.ino
+++ bluetooth.ino
@@ -5,22 +5,39 @@
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-class MyServerCallbacks : public BLEServerCallbacks
+class ServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *pServer)
   {
-    deviceConnected = true;
-  };
+    Serial.println("Device connected");
+  }
 
   void onDisconnect(BLEServer *pServer)
   {
-    deviceConnected = false;
+    Serial.println("Device disconnected");
+  }
+};
+
+class CharacteristicCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    String value = pCharacteristic->getValue();
+
+    if (value.length() > 0)
+    {
+      Serial.println("Received value: ");
+      for (int i = 0; i < value.length(); i++)
+      {
+        Serial.print(value[i]);
+      }
+
+      Serial.println();
+    }
   }
 };
 
@@ -29,11 +46,11 @@ void setup()
   Serial.begin(115200);
 
   // Create the BLE Device
-  BLEDevice::init("ESP32 Test");
+  BLEDevice::init("Scoobies Penguin");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+  pServer->setCallbacks(new ServerCallbacks());
 
   // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -46,6 +63,8 @@ void setup()
           BLECharacteristic::PROPERTY_NOTIFY |
           BLECharacteristic::PROPERTY_INDICATE);
 
+  pCharacteristic->setCallbacks(new CharacteristicCallbacks());
+
   // Start the service
   pService->start();
 
@@ -56,11 +75,10 @@ void setup()
   pAdvertising->setMinPreferred(0x06);
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  Serial.println("Waiting for a client connection to notify...");
+
+  Serial.println("BLE device ready");
 }
 
 void loop()
 {
-  // Put your main code here, to run repeatedly:
-  delay(2000);
 }
